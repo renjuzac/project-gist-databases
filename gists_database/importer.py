f5980fe59649f3b530bee61746d16ffd329510d5
renjuzac <renjuzac@gmail.com>
2018-06-04 04:26:42 +0000

    All tests passing

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,26 @@
 import requests
+from datetime import datetime
+
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    url = "https://api.github.com/users/{username}/gists"
+
+    response  = requests.get(url.format(username=username))   
+    if response.status_code != 200 :
+        raise requests.exceptions.HTTPError
+        
+    c = db.cursor()
+    for entry  in response.json() :
+        # print(entry.values())
+        sql = 'INSERT INTO gists (github_id,html_url ,git_pull_url ,git_push_url,commits_url,forks_url ,public ,created_at ,updated_at,comments,comments_url) values (?,?,?,?,?,?,?,?,?,?,?)'
+        c.execute(sql,(entry['id'],entry['html_url'],entry['git_pull_url'],entry['git_push_url'], entry['commits_url'],entry['forks_url'],entry['public'],entry['created_at'],entry['updated_at'],entry['comments'],entry['comments_url']))
+        print(entry['created_at'])
+        
+    if commit:
+        db.commit()
+
+  
+
+# dict_keys(['url', 'html_url', 'git_push_url', 'description', 'comments_url', 'updated_at', 'user', 'owner', 'truncated', 'files', 'commits_url', 'id', 'git_pull_url', 'created_at', 'public', 'forks_url', 'comments'])
+    
+    
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,28 @@
 from .models import Gist
+from datetime import datetime
 
 def search_gists(db_connection, **kwargs):
-    pass
+    c = db_connection.cursor()
+    sql = ''
+ 
+    if 'github_id' in kwargs and 'created_at' in kwargs:
+        sql = 'SELECT * from gists WHERE datetime(created_at)==datetime(?) AND github_id==(?)'
+        c.execute(sql,(kwargs['created_at'],kwargs['github_id'],))
+    elif 'github_id' in kwargs:    
+        sql = 'SELECT * from gists WHERE github_id==(?)'
+        print(kwargs['github_id'])
+        c.execute(sql,(kwargs['github_id'],))
+    elif 'created_at' in kwargs:    
+        sql = 'SELECT * from gists WHERE datetime(created_at)==datetime(?)'
+        c.execute(sql,(kwargs['created_at'],))
+        
+        # datetime.strptime(entry['created_at'],"%Y-%m-%dT%H:%M:%SZ")
+    else:
+        sql = 'SELECT * from gists'
+        c.execute(sql)
+    result = c.fetchall()
+    gistlist  = []
+    for gist in result:
+        gistlist.append(Gist(gist))
+    return gistlist
+
